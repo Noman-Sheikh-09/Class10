bc64f5724bb1a719657f906a2ce10965c93f4588
noman <nomanshk310@gmail.com>
2021-12-13 01:49:23 +0500

    Date Handling Practice

---
+++ script.js
@@ -0,0 +1,28 @@
+// var date = new Date()
+// console.log("Date", date);
+// var weekDay = date.getDay()
+
+// var days = ['sun', 'Mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+// console.log("weekDay", weekDay , days[weekDay]);
+
+// var getHours =  date.getHours()
+// console.log("Hours",getHours);
+
+// var getMinutes = date.getMinutes()
+// console.log("Minutes",getMinutes);
+// var getSeconds = date.getSeconds()
+// console.log("Seconds",getSeconds);
+// var getMonths = date.getMonth()
+// console.log("Month",getMonths);
+// var getYear = date.getFullYear()
+// console.log("getYear",getYear);
+// var getTime = date.getTime()
+// console.log("getDate",getTime);
+
+// var today = new Date();
+// // console.log( today);
+// var otherDate = new Date('01-06-2005')
+
+// console.log(otherDate, otherDate.setDate(12));
+const d = new Date("October 13, 2014 11:13:00");
+document.getElementById("demo").innerHTML = d;
